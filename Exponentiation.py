2e8e0b69cce99fb02f52f5fa4a8a99008a37706f
Katerina Paparizou <76427845+Katerinajames@users.noreply.github.com>
2024-10-24 22:36:54 +0300

    Add files via upload

---
+++ Exponentiation.py
@@ -0,0 +1,9 @@
+
+def exp(base,exponent):
+	result=base**exponent
+	return result
+print("---------------------------------------")
+exponent=4
+base= 3 
+for i in range (1,exponent+1):
+	print( exp(base,i))
